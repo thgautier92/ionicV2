34b9488e90196422344d7ddd047c4a597be35ccf
Thierry GAUTIER <th.gautier92@gmail.com>
2016-05-06 21:51:21 +0200

    Delte unused files

--- app/pages/app-browser/app-browser.ts
+++ app/pages/app-browser/app-browser.ts
@@ -10,27 +10,20 @@ import {Page, NavController} from 'ionic-angular';
   templateUrl: 'build/pages/app-browser/app-browser.html',
 })
 export class AppBrowserPage {
-  lstUrl:any;
-  options:any;
+  lstUrl: any;
+  options: any;
   constructor(public nav: NavController) {
     this.lstUrl = [
-    { "id": 1, "title": "Le Monde", "url": "http://www.lemonde.fr" },
-    { "id": 2, "title": "Jdnet", "url": "http://www.journaldunet.com/" },
-    { "id": 3, "title": "01.net", "url": "http://www.01net.com/" }];
-    this.options= {
+      { "id": 1, "title": "Le Monde", "url": "http://www.lemonde.fr","img":"http://www.lemonde.fr//medias/web/76f5986997539210ea49c34b9a19a243/img/sprites/sprite.png"},
+      { "id": 2, "title": "Jdnet", "url": "http://www.journaldunet.com/" },
+      { "id": 3, "title": "01.net", "url": "http://www.01net.com/" }];
+    this.options = {
       "location": "no",
       "clearcache": "no",
       "toolbar": "no"
     };
   }
   openUrl(url) {
-            $window.open(url, '_self', this.options)
-                .then(function (event) {
-                    // success
-                })
-                .catch(function (event) {
-                    // error
-                });
-            //$cordovaInAppBrowser.close();
-        };
+    window.open(url, '_blank', this.options);
+  };
 }
