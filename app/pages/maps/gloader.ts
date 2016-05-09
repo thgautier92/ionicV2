7791f650e61a2926f28b7bd81e2b5c3de70f508c
Thierry GAUTIER <th.gautier92@gmail.com>
2016-05-09 13:51:34 +0200

    Maps Markers

--- app/pages/maps/gloader.ts
+++ app/pages/maps/gloader.ts
@@ -1,11 +1,16 @@
+import {Geolocation} from 'ionic-native';
+import {Injectable} from 'angular2/core';
 //const url = 'https://apis.google.com/js/client.js?onload=__onGoogleLoaded'
 const gmapKey = 'AIzaSyDsfocxKIufTQdkZ7lXe-bgYThcXF2jkZY'
-const gmapUrl = 'https://maps.googleapis.com/maps/api/js?key='+gmapKey+'&callback=__onGoogleLoaded'
+const gmapUrl = 'https://maps.googleapis.com/maps/api/js?key=' + gmapKey + '&callback=__onGoogleLoaded'
 
-
+@Injectable()
 export class GoogleAPI {
   loadAPI: Promise<any>
   map: any;
+  optionsGeoLoc: any;
+  defaultLoc: any;
+  currentLoc: any;
   constructor() {
     this.loadAPI = new Promise((resolve) => {
       window['__onGoogleLoaded'] = (ev) => {
@@ -14,14 +19,11 @@ export class GoogleAPI {
       }
       this.loadScript()
     });
+    this.optionsGeoLoc = { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true };
+    this.defaultLoc = { lat: 48.8534100, lng: 2.3488000 };
+    this.map = null;
 
   }
-  loadMap() {
-    return this.loadAPI.then((map) => {
-      return map;
-    });
-  }
-
   loadScript() {
     console.log('Loading Google API...')
     let node = document.createElement('script');
@@ -29,4 +31,53 @@ export class GoogleAPI {
     node.type = 'text/javascript';
     document.getElementsByTagName('head')[0].appendChild(node);
   }
+  mapLoad() {
+    return this.loadAPI.then((map) => {
+      return map;
+    });
+  }
+  geoLocation() {
+    return new Promise((resolve) => {
+      Geolocation.getCurrentPosition(this.optionsGeoLoc).then((resp) => {
+        //console.log(resp);
+        this.currentLoc = resp;
+        resolve(this.currentLoc);
+      }, error => {
+        this.currentLoc = this.defaultLoc;
+        resolve(this.currentLoc);
+      });
+
+    });
+  }
+  mapCenter(map, pos) {
+    // pos format : {lat:xxx,lng:yyy}
+    //console.log("Center to the position", pos, map);
+    map.setCenter(pos);
+  }
+  mapAddMarker(map, pos, markerText) {
+    var image = {url: 'img/mapPositionPin.png',size:null,origin:null,anchor:null};
+      //image.size= new google.maps.Size(20,32);
+      //image.origin= new google.maps.Point(0, 0);
+      //image.anchor= new google.maps.Point(0, 5);
+      //icon: image,animation: google.maps.Animation.BOUNCE,
+    return new Promise((resolve) => {
+      let marker = new google.maps.Marker({
+        position: pos,     
+        map: map,
+        draggable: true
+      })
+      let infowindow = new google.maps.InfoWindow({
+        content: markerText,
+      });
+      marker.addListener('click', function () {
+        marker.setAnimation(null);
+        infowindow.open(map, marker);
+      });
+      resolve(marker);
+    });
+  }
+  mapRemoveMarker(marker) {
+    marker.setMap(null);
+  }
+
 }
--- app/pages/maps/maps.ts
+++ app/pages/maps/maps.ts
@@ -1,5 +1,7 @@
 import {Page, NavController} from 'ionic-angular';
 import {GoogleAPI} from './gloader'
+import {DisplayTools} from '../comon/display';
+
 
 /*
   Generated class for the MapsPage page.
@@ -9,18 +11,22 @@ import {GoogleAPI} from './gloader'
 */
 @Page({
   templateUrl: 'build/pages/maps/maps.html',
-  providers: [GoogleAPI]
+  providers: [GoogleAPI,DisplayTools]
 })
 export class MapsPage {
+  display:any;
   gmap: any;
   map: any;
   gLoad: any;
-  constructor(public nav: NavController, gLoad: GoogleAPI) {
+  options: any;
+  markerCurrentPos:any;
+  constructor(public nav: NavController, gLoad: GoogleAPI, display:DisplayTools) {
+    this.display = display;
     this.gLoad = gLoad;
     this.gmap = {
       params: { lat: 48.8534100, lng: 2.3488000 }
     };
-    gLoad.loadMap().then((mapApi) => {
+    this.gLoad.mapLoad().then((mapApi) => {
       console.log('Map API Loaded', mapApi);
       this.initMap(mapApi);
     })
@@ -32,11 +38,33 @@ export class MapsPage {
     let mapOptions = {
       center: latLng,
       zoom: 15,
+      zoomControl: true,
+      mapTypeControl: true,
+      scaleControl: true,
+      streetViewControl: true,
+      rotateControl: true,
       mapTypeId: mapApi.maps.MapTypeId.ROADMAP
     }
 
     this.map = new mapApi.maps.Map(document.getElementById("map"), mapOptions);
-    console.log(this.map);
+    this.map.setTilt(45);
+    //console.log(this.map);
+  }
+  centerLoc() {
+    let loading = this.display.displayLoading("Géolocalisation en cours...");
+    if (this.markerCurrentPos) {
+      this.gLoad.mapRemoveMarker(this.markerCurrentPos);
+    }
+    this.gLoad.geoLocation().then((pos) => {
+      //console.log("Position : ", pos);
+      let p = { lat: pos.coords.latitude, lng: pos.coords.longitude };
+      this.gLoad.mapAddMarker(this.map, p, "Vous êtes ici").then((marker) => {
+        console.log("Marker", marker);
+        this.markerCurrentPos=marker;
+        this.gLoad.mapCenter(this.map, p);
+        loading.dismiss();
+      });
+    })
   }
 }
 
