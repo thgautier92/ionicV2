489a0f8da5ec08586ab77049a1581a9808b72e5b
Thierry GAUTIER <th.gautier92@gmail.com>
2016-06-08 20:10:59 +0200

    Add MFP call adapaters

--- app/pages/api/mfp-api/explore/explore.ts
+++ app/pages/api/mfp-api/explore/explore.ts
@@ -1,6 +1,9 @@
-import {Page, NavController} from 'ionic-angular';
+import {Page, NavController,Slides} from 'ionic-angular';
+import {DisplayTools} from '../../../comon/display';
+
 declare var WL: any;
-declare var WLAuthorizationManager:any; 
+declare var WLAuthorizationManager: any;
+declare var WLResourceRequest: any;
 /*
   Generated class for the ExplorePage page.
 
@@ -9,15 +12,55 @@ declare var WLAuthorizationManager:any;
 */
 @Page({
   templateUrl: 'build/pages/api/mfp-api/explore/explore.html',
+  providers: [DisplayTools]
 })
 export class ExplorePage {
-  constructor(public nav: NavController) {}
-  getToken() {
-    WLAuthorizationManager.obtainAccessToken().then(function(token) {
-        alert("success: "+JSON.stringify(token))
-    },function(error) {
-        alert("failure: "+JSON.stringify(error))
+  errorMsg: any;
+  infoSrv: any;
+  dataRss: any;
+  items:any;
+  slideOptions:any;
+  display:DisplayTools;
+  constructor(public nav: NavController,display:DisplayTools) {
+    this.display = display;
+    this.infoSrv = "";
+    this.dataRss = null;
+    this.items = null;
+    this.errorMsg = "";
+    this.slideOptions = {
+      initialSlide: 1,
+      loop: true
+    };
+  }
+  next() {
+
+  };
+  getSrvUrl() {
+    this.display.displayLoading("Interrogation du serveur Mobile First",1);
+    let me = this;
+    me.infoSrv="";
+    WL.App.getServerUrl(function(response){
+      me.infoSrv=response;
+    }, function(error){
+      me.errorMsg = JSON.stringify(error);
     });
-};
+  };
+  getAdapt() {
+    this.display.displayLoading("Lecture RSS",1);
+    let me = this;
+    let request = new WLResourceRequest("adapters/rss/getFeed", WLResourceRequest.GET);
+    request.send().then(
+      function (response) {
+        me.dataRss = response;
+        me.items=response.rss.chanel.item
+        console.log("MFP => RSS return",response);
+      },
+      function (error) {
+        alert("failure: "+JSON.stringify(error));
+        me.errorMsg = JSON.stringify(error);
+      }
+    );
+
+  }
 }
 
--- app/pages/api/mfp-api/infos/infos.ts
+++ app/pages/api/mfp-api/infos/infos.ts
@@ -1,5 +1,8 @@
 import {Page, NavController} from 'ionic-angular';
+import {DisplayTools} from '../../../comon/display';
 
+declare var WL: any;
+declare var WLAuthorizationManager: any;
 /*
   Generated class for the InfosPage page.
 
@@ -8,7 +11,28 @@ import {Page, NavController} from 'ionic-angular';
 */
 @Page({
   templateUrl: 'build/pages/api/mfp-api/infos/infos.html',
+  providers: [DisplayTools]
 })
 export class InfosPage {
-  constructor(public nav: NavController) {}
+  errorMsg: any;
+  infoSrv: any;
+  display:DisplayTools;
+  constructor(public nav: NavController,display:DisplayTools) {
+    this.infoSrv = "";
+    this.errorMsg = "";
+    this.display = display;
+  }
+  getToken() {
+    //console.log("MFP => Test Server,get token");
+    this.display.displayLoading("Accés au serveur Mobile First",1);
+    let me = this;
+    me.infoSrv="";
+    WLAuthorizationManager.obtainAccessToken().then(function (token) {
+      me.infoSrv = token['value'];
+      //console.log("MFP => Token",JSON.stringify(token));
+    }, function (error) {
+      me.errorMsg = JSON.stringify(error);
+      //alert("failure: "+JSON.stringify(error))
+    });
+  };
 }
